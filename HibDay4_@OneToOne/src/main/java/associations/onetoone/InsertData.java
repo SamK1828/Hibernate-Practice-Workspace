6da9f26a20878aa0414d0f0cda1bafbbed169fef
Samarth Deelip Kalegaonkar <kalegaonkarsamarth@gmail.com>
2025-08-07 01:23:18 +0530

    Added/Updated OneToOne mapping

---
+++ HibDay4_@OneToOne/src/main/java/associations/onetoone/InsertData.java
@@ -0,0 +1,44 @@
+package associations.onetoone;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import associations.entity.Laptop;
+import associations.entity.Student;
+
+
+
+public class InsertData {
+
+	public static void main(String[] args) {
+
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		cfg.addAnnotatedClass(Student.class);
+		cfg.addAnnotatedClass(Laptop.class);
+
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session ss = sf.openSession();
+		Transaction tr = ss.beginTransaction();
+
+		Laptop l = new Laptop();
+		l.setL_id(210000);
+		l.setL_name("HP");
+		ss.persist(l);
+
+		Student s = new Student();
+		s.setName("rahul");
+		s.setAge(33);
+		s.setLaptop(l);
+
+		ss.persist(s);
+		tr.commit();
+		ss.close();
+
+		System.out.println("Data is inserted...");
+
+	}
+
+}
